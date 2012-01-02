65985a5cf6b6db88fdec83e517e76fc23eee3dff
Asia Biega <asia.biega@gmail.com>
2012-01-02 23:03:54 +0100

    AST moved to separate file

--- Evaluator.hs
+++ Evaluator.hs
@@ -1,29 +1,7 @@
+module Evaluator where
 import Prelude
 import Data.List(elem)
-
--------------------------------------------------------------------------------------------------------------
-
-data Term = TmIf Condition Term [(Condition, Term)] Term
-            | TmDecision NewState Utterance
-            | TmCase Variable [(ValueSet, Term)] Term
-            deriving(Eq, Show)
-
-data Condition = TmEquals Variable Int | TmAnd [Condition] | TmOr [Condition] | TmTrue | TmFalse
-            deriving(Eq, Show)
-
-data NewState = TmCurrent | TmState Int
-            deriving(Eq, Show)
-
-data Variable = TmVar String
-            deriving(Eq, Show)
-
-type StateNumber = Int
-type ValueSet = [Int]
-type Utterance = String
-
-type Rule = ([StateNumber], Term)
-type Character = [Rule]
-type Env = [(String, Int)]
+import AST
 
 -------------------------------------------------------------------------------------------------------------
 
