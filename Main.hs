80261f48e87ae77fb315b9a93e2d90bf7c2322d7
Aleksander Balicki <balicki.aleksander@gmail.com>
2012-01-03 15:46:21 +0100

    some timeout gizmos

--- Main.hs
+++ Main.hs
@@ -1,4 +1,17 @@
 import Prelude hiding (lex)
+import System.Timeout
+import System.Environment
+import System.IO
+import System.Exit
+
 import Lexer
 import Parser
-main = getContents >>= print . parse . lex
+
+main = do
+    (timeArg:args) <- getArgs
+    let time = read timeArg
+    cont <- getContents
+    ast <- timeout (time*1000000) (return $ parse . lex $ cont)
+    case ast of
+        Nothing -> hPutStrLn stderr "timed out" >> exitFailure
+        Just goodAst -> print goodAst
