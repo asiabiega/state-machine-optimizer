c966f4a72d882a7b3d0ba37d8b38680302259166
Aleksander Balicki <balicki.aleksander@gmail.com>
2012-01-03 19:09:15 +0100

    vars

--- AST.hs
+++ AST.hs
@@ -1,5 +1,7 @@
 module AST where
 
+import Data.List
+
 data Term = TmIf Condition Term [(Condition, Term)] Term
             | TmDecision NewState Utterance
             | TmCase Variable [(ValueSet, Term)] Term
@@ -22,3 +24,21 @@ type Cost = Float
 type Rule = ([StateNumber], Term)
 type Character = [Rule]
 type Env = [(String, Int)]
+
+
+vars :: Character -> [String]
+vars rules = vunion $ map varsRule rules where
+    varsRule (_, term) = varsTerm term
+
+    varsTerm (TmIf cnd t1 elseifs t2) = vunion $ map varsTerm ([t1, t2] ++ map snd elseifs) ++ map varsCnd (cnd : map fst elseifs)
+    varsTerm (TmDecision _ _) = []
+    varsTerm (TmCase (TmVar v) arms t) = v : (vunion $ map varsTerm $ t : map snd arms)
+
+    varsCnd (TmEquals (TmVar v) _) = [v]
+    varsCnd (TmAnd cnds) = vunion $ map varsCnd cnds
+    varsCnd (TmOr cnds) = vunion $ map varsCnd cnds
+    varsCnd TmTrue = []
+    varsCnd TmFalse = []
+
+vunion :: Eq a => [[a]] -> [a]
+vunion = foldl' union []
