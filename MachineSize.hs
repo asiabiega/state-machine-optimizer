9fa5093c381d061d1bf61710e6462dccd9ff9ec6
Aleksander Balicki <balicki.aleksander@gmail.com>
2012-01-05 20:14:10 +0100

    indentation fixed to compile again

--- MachineSize.hs
+++ MachineSize.hs
@@ -5,9 +5,8 @@ import AST
 
 msize character = foldl (+) 0 (map (msizeTerm.snd) character)
 
-msizeTerm (TmIf cond tt elifs tf) = let msize_elifs = 
-                                        foldl (+) 0 (map (\(x,y) -> (msizeCondition x) + (msizeTerm y)) elifs)
-                                    in (msizeCondition cond) + (msizeTerm tt) + (msize_elifs) + (msizeTerm tf)
+msizeTerm (TmIf cond tt elifs tf) = let msize_elifs = foldl (+) 0 (map (\(x,y) -> (msizeCondition x) + (msizeTerm y)) elifs)
+    in (msizeCondition cond) + (msizeTerm tt) + (msize_elifs) + (msizeTerm tf)
 msizeTerm (TmDecision TmCurrent _) = 3
 msizeTerm (TmDecision _ _) = 4
 msizeTerm (TmCase v arms def) = let msize_arms = 
@@ -15,9 +14,8 @@ msizeTerm (TmCase v arms def) = let msize_arms =
                                     in 10 + msize_arms + (mspan arms) + (msizeTerm def)
 
 
-mspan arms = let all_labels = 
-                foldl (++) [] (map (\x -> fst x) arms)
-            in (maximum all_labels) - (minimum all_labels) + 1
+mspan arms = let all_labels = foldl (++) [] (map (\x -> fst x) arms)
+    in (maximum all_labels) - (minimum all_labels) + 1
 
 
 
